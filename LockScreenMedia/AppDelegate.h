7e5b849224ccddc438af21449721dfb4156d3b45
ylang <yangjw.kai7@gmail.com>
2015-11-18 09:24:26 +0800

    Initial Commit

---
+++ LockScreenMedia/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LockScreenMedia
+//
+//  Created by njdby on 15/11/18.
+//  Copyright © 2015年 njdby. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LockScreenMedia/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LockScreenMedia
+//
+//  Created by njdby on 15/11/18.
+//  Copyright © 2015年 njdby. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
